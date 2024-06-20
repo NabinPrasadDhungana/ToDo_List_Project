935a42387af6daae0851c435e3d82ceb7cd9b4d7
Nabin Prasad Dhungana <prnabin123@gmail.com>
2024-06-20 20:24:22 +0545

    Navbar added

--- todolist_project/todolist_app/urls.py
+++ todolist_project/todolist_app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
+from . import views
 
 urlpatterns = [
-    
+    path('', Home.as_view(), name='home')
 ]
