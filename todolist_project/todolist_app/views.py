b96e35fdb8359031c01399527d7bfaebe3af183b
Nabin Prasad Dhungana <prnabin123@gmail.com>
2024-06-21 18:43:35 +0545

    Todo items search feature added

--- todolist_project/todolist_app/views.py
+++ todolist_project/todolist_app/views.py
@@ -1,5 +1,8 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from .models import *
 # Create your views here.
 
 # def home(request):
@@ -7,3 +10,14 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('search')
+
+        if query:
+            context['todo_items'] = ToDoItem.objects.filter(title__icontains=query)
+        else:
+            context['todo_items'] = ToDoItem.objects.all()
+
+        return context
